67afafffc14a3c9c38626fcd88ec8eea6d1176e1
Александр <vrotmnenogi.12@mail.ru>
2023-03-02 20:58:09 +0400

    Добавляет компонент "интерактивное меню".
    Подправлены отступы Футера.
    
    Добавлена дополнительная скрываемая навигация.
    
    Изменена вёрстка Хедера (для большей гибкости).

---
+++ scripts/javascript.js
@@ -0,0 +1,8 @@
+// Появляющийся по клику блок
+const navButtonDisplay = document.getElementsByClassName('navigation-item-button') [0]
+const hiddenCatalogList = document.getElementsByClassName('hidden-catalog-list') [0]
+
+// Добавляет обработчик на событие клик
+navButtonDisplay.addEventListener('click', function () {
+  hiddenCatalogList.classList.toggle('hidden')
+})
