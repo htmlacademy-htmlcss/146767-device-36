d5be4d7011a3114d4a67054ae6731db18be9c230
Александр <vrotmnenogi.12@mail.ru>
2023-03-11 22:11:17 +0400

    Изменяет меню. Добавляет ссылки
    Добавлены внутренние ссылки.
    Изменено меню для большего соответствия ТЗ.
    Добвален лейбл "new" в каталоге.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,9 +1,10 @@
 // Появляющийся по клику блок
-const navButtonDisplay = document.getElementsByClassName('navigation-item-button') [0]
+const navButtonDisplay = document.getElementsByClassName('wrapper-navigation-button') [0]
+const navButtonIcon = document.getElementsByClassName('navigation-button-icon') [0]
 const hiddenCatalogList = document.getElementsByClassName('hidden-catalog-list') [0]
 
 // Добавляет обработчик на событие клик
 navButtonDisplay.addEventListener('click', function () {
   hiddenCatalogList.classList.toggle('hidden')
-  navButtonDisplay.classList.toggle('show')
+  navButtonIcon.classList.toggle('show')
 });
